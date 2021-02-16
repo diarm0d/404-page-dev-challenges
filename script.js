f776ff36063a599f6f196d0e6e471503d5d6d51e
diarm0d <diarmuid.odonovan@protonmail.com>
2021-02-16 09:58:46 +0000

    Updated design layout, removed js functionality

--- script.js
+++
@@ -1,9 +0,0 @@
-const clientWidthNumber = document.documentElement.clientWidth;
-const clientHeightNumber = document.documentElement.clientHeight;
-
-function showMetheMoney() {
-    console.log('height:',clientHeightNumber, 'width:', clientWidthNumber)
-}
-
-clientWidthNumber.addEventListener('change', showMetheMoney);
-clientHeightNumber.addEventListener('change', showMetheMoney);
